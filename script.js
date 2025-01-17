92f3142bcb06d7eba833900bd02ecce5429b671d
stacy0512 <rudneva.anastasia@gmail.com>
2025-01-16 19:31:42 -0700

    Formatted files, added hamburger menu, css file

---
+++ script.js
@@ -0,0 +1,66 @@
+<script>
+document.getElementById("logo").innerHTML = "JavaScript";
+document.getElementById("logo").style.color = "red";
+
+// This is JS comment
+
+//document.getElementById("logo").style.display = "none";
+
+document.getElementById("logo").classList.add("new class");
+
+//document.getElementsByClassName("work-process").style.color = "2px solid blue";
+
+var example = 'example value';
+
+console.log(example);
+
+var logo = document.getElementById("logo");
+
+let new_logo = "this is new stuff"
+
+const logo = document.getElementById
+
+
+logo.innerHTML = "New Values"
+
+console.log(new_logo, 'first time')
+
+new_logo = "different value"    
+
+let a = 5;
+let b = 25;
+
+let sum = a+b;
+
+let multi = a * b;
+
+console.log(sum)
+
+console.log(multi)
+
+for (initialize; condition; increment (decrement)){
+    //Action to execute
+}
+
+
+for (let loop_init = 1; loop_init <=3; loop_init++ ) {
+    console.log('for loop' loop_init)
+}
+
+
+var name = 'Artur'
+
+//Declared the function
+function greet(name) {
+   
+   console.log('Hello', name)
+    
+}
+
+//called the functiom
+greet('Artur');
+
+
+<script> script src(script.js) </script>
+
+</script>
---
+++ working-scripts.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+const hamburger = document.getElementById('hamburger-id');
+const menu = document.getElementById('menu-id');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('show')
+})
+
+})
